ff254ab23ad02f9ff36c1900d5bad6a921ebd9c4
阿相 <485972440@qq.com>
2022-07-08 10:47:11 +0800

    json

--- apm-util/src/main/java/com/antworking/util/JsonUtil.java
+++ apm-util/src/main/java/com/antworking/util/JsonUtil.java
@@ -7,4 +7,7 @@ public class JsonUtil {
     public static String toJsonString(Object o){
         return new GsonBuilder().disableHtmlEscaping().create().toJson(o);
     }
+    public static <T> T fromJson(String json,Class<T> clazz){
+        return new GsonBuilder().disableHtmlEscaping().create().fromJson(json,clazz);
+    }
 }
